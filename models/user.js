75e6a810c2a640e2842d3e011024063194586ac5
Aki Lempola <aki.lempola@tuni.fi>
2020-11-10 14:04:07 +0200

    Task 9.4 done

--- models/user.js
+++ models/user.js
@@ -2,8 +2,41 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
 const userSchema = new Schema({
   // TODO: 9.4 Implement this
+  name: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: EMAIL_REGEX
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 10,
+    set: v => {
+      if (v.length >= 10) {
+        return bcrypt.hashSync(v, 10);
+      }
+    }
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'customer'],
+    default: 'customer',
+    trim: true,
+    lowercase: true
+  }
 });
 
 /**
@@ -14,6 +47,7 @@ const userSchema = new Schema({
  */
 userSchema.methods.checkPassword = async function (password) {
   // TODO: 9.4 Implement this
+  return bcrypt.compare(password, this.password);
 };
 
 // Omit the version key when serialized to JSON
