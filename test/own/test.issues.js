1641d652d20e19d66c7dcf205505c9a1f626e055
Teemu Mökkönen <teemu.mokkonen@tuni.fi>
2020-11-23 13:23:20 +0200

    10.3 maybe done

---
+++ test/own/test.issues.js
@@ -0,0 +1,53 @@
+// JavaScript source code
+
+describe('Issues', () => {
+    //first
+    it('As an admin I want to add new a product to the database.', async () => {
+        expect(false).to.be.true;
+    });
+    //second
+    it('As an admin I want to edit a product.', async () => {
+        expect(false).to.be.true;
+    });
+    //third
+    it('As an admin I want to delete a product.', async () => {
+        expect(false).to.be.true;
+    });
+
+    //fourth
+    it('As a customer I want to view my orders.', async () => {
+        expect(false).to.be.true;
+    });
+
+    //fifth
+    it('As an admin I want to view all orders.', async () => {
+        expect(false).to.be.true;
+    });
+
+    //sixth
+    it('As an admin/customer I want to view details of an order.', async () => {
+        expect(false).to.be.true;
+    });
+
+    //seventh
+    it('As a customer I want to make an order.', async () => {
+        expect(false).to.be.true;
+    });
+
+    //eight
+    it('Create a schema for products', async () => {
+        expect(false).to.be.true;
+    });
+
+    //ninth
+
+    it('Create a schema for orders', async () => {
+        expect(false).to.be.true;
+    });
+
+    //tenth
+
+    it('Create a schema for orderedItem', async () => {
+        expect(false).to.be.true;
+    });
+});
